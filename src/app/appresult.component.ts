fa43f3360cb2a7d75c197e25d01d4e3fbaa862bf
sasaco <sasaicco@gmail.com>
2020-08-21 11:31:32 +0900

    jExcel を使うと思うから入れといた

--- src/app/appresult.component.ts
+++ src/app/appresult.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { AppComponent } from './app.component';
+
+import * as jexcel from 'jexcel';
 
 @Component({
   selector: 'appresult-root',
@@ -13,5 +15,17 @@ export class AppResultComponent {
 
   }
 
+  @ViewChild("spreadsheet") spreadsheet: ElementRef;
+
+  ngAfterViewInit() {
+    jexcel(this.spreadsheet.nativeElement, {
+      data: [[]],
+      columns: [
+        { type: "dropdown", width: "100px", source: ["Y", "N"] },
+        { type: "color", width: "100px", render: "square" }
+      ],
+      minDimensions: [10, 10]
+    });
+  }
 
 }
