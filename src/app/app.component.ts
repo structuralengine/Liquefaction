71fb5262542bc43241c5062dfddd91d64fc4b65d
sasaco <sasaicco@gmail.com>
2020-08-29 08:49:29 +0900

    ファイル保存・開くボタンを右上にしてみた

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { SaverdataService } from './saverdata/saverdata.service'
+
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +12,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  constructor() {
+  constructor(private router: Router,
+              public sd: SaverdataService) {
 
+  }
+
+  save(): void {
+    const data: string = this.sd.save();
+    const blob = new window.Blob([data], { type: 'text/plain' });
+    FileSaver.saveAs(blob, 'liquef.liq');
+  }
+
+  // ファイルを開く
+  open(evt) {
+    const file = evt.target.files[0];
+    evt.target.value = '';
+    this.fileToText(file)
+      .then(text => {
+        this.sd.load(text);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+      this.router.navigate(['/explain']);
+  }
+
+  private fileToText(file): any {
+    const reader = new FileReader();
+    reader.readAsText(file);
+    return new Promise((resolve, reject) => {
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.onerror = () => {
+        reject(reader.error);
+      };
+    });
   }
 
 
--- src/app/appinput-underground.component.ts
+++ src/app/appinput-underground.component.ts
@@ -65,39 +65,6 @@ export class appunderground implements OnInit, OnDestroy{
     });
   }
 
-  save(): void {
-    const data: string = this.sd.save();
-    const blob = new window.Blob([data], { type: 'text/plain' });
-    FileSaver.saveAs(blob, 'liquef.liq');
-  }
-
-  // ファイルを開く
-  open(evt) {
-    const file = evt.target.files[0];
-    evt.target.value = '';
-    this.fileToText(file)
-      .then(text => {
-        this.sd.load(text);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-      this.router.navigate(['/explain']);
-  }
-
-  private fileToText(file): any {
-    const reader = new FileReader();
-    reader.readAsText(file);
-    return new Promise((resolve, reject) => {
-      reader.onload = () => {
-        resolve(reader.result);
-      };
-      reader.onerror = () => {
-        reject(reader.error);
-      };
-    });
-  }
-
   
 
 }
